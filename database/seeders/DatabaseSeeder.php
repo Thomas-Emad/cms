3096d2473f19ec21da1de00478ea01a831be6793
thomas-emad-smarttel <thomas.emad@smarttel.com.eg>
2026-09-22 09:22:02 +0300

    edit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([see
+        $this->call([
             HotelSeeder::class,
             DemoContentSeeder::class,
             PageSeeder::class,
